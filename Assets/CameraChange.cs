567b192aac8d6099c1b16131e602d4193c82db01
Nicole Figg <Nikkistone@users.noreply.github.com>
2020-12-06 17:44:59 -0800

    Basic Setup and Level Design

---
+++ Assets/CameraChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+
+    public Camera FirstPersonCam;
+    public Camera ThirdPersonCam;
+    bool firstpersonCam = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstPersonCam.enabled = firstpersonCam;
+        ThirdPersonCam.enabled = !firstpersonCam;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            firstpersonCam = !firstpersonCam;
+            FirstPersonCam.enabled = firstpersonCam;
+            ThirdPersonCam.enabled = !firstpersonCam;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public int pointsTotal = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        // Character controller movement
+        var characterController = this.GetComponent<CharacterController>();
+        characterController.SimpleMove(new Vector3(
+            Input.GetAxis("Horizontal") * this.speed,
+            0.0f,
+            Input.GetAxis("Vertical") * this.speed
+            ));
+
+    }
+}
